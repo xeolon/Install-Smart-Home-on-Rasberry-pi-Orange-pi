0556cc46275bb91b3ea9c24dcd104c14fecfd4b3
xeolone2009 <xeolon237@gmail.com>
2024-08-12 21:33:13 +0700

    Initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#cài đặt các gói cần thiết
+sudo apt update -y; sudo apt upgrade -y; sudo apt install git
+
+#cài đặt python
+sudo apt install python3 -y;
+sudo apt install python3-pip -y;
+
+# kiểm tra python
+sudo python3 -V ; sudo pip3 -V
+
+#cài đặt smarthome
+
+
